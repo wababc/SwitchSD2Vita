128b2f731515b5a43127d00bdca6d83392eeec37
wababc <36303163+wababc@users.noreply.github.com>
2018-08-29 20:03:46 +0100

    Add files via upload
    V2.1Mod by wababc

---
+++ cmake-build-debug/CMakeFiles/emulator.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/emulator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/emulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/switch_sd2vita.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/ouajdibelhaj/CLionProjects/SwitchSD2VitaMod/src/Engine.cpp" "/Users/ouajdibelhaj/CLionProjects/SwitchSD2VitaMod/cmake-build-debug/CMakeFiles/switch_sd2vita.dir/src/Engine.cpp.obj"
+  "/Users/ouajdibelhaj/CLionProjects/SwitchSD2VitaMod/src/File.cpp" "/Users/ouajdibelhaj/CLionProjects/SwitchSD2VitaMod/cmake-build-debug/CMakeFiles/switch_sd2vita.dir/src/File.cpp.obj"
+  "/Users/ouajdibelhaj/CLionProjects/SwitchSD2VitaMod/src/Menu.cpp" "/Users/ouajdibelhaj/CLionProjects/SwitchSD2VitaMod/cmake-build-debug/CMakeFiles/switch_sd2vita.dir/src/Menu.cpp.obj"
+  "/Users/ouajdibelhaj/CLionProjects/SwitchSD2VitaMod/src/main.cpp" "/Users/ouajdibelhaj/CLionProjects/SwitchSD2VitaMod/cmake-build-debug/CMakeFiles/switch_sd2vita.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../common"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
